712bfba1d7b04459398bf4e9805d521d7df33bfd
Dustin <gjf66924011@gmail.com>
2023-11-23 18:33:42 +0800

    添加背景

---
+++ assets/script/Block.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Block')
+export class Block extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
